2f821755288c171ca551fe237eb2e00a989cdacc
kkchamath17 <kk.chamath@hotmail.com>
2021-08-25 00:12:11 +0530

    login fix harcore

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['username']))
-{
-    header("Location: login.php");
-    die();
-}?>
+    header("Location: login.php");?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
