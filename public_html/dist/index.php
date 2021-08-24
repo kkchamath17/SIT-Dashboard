7f1bcae41c7a3851ad2e3a28c9b85ab40a02fa3d
kkchamath17 <kk.chamath@hotmail.com>
2021-08-25 00:28:18 +0530

    cahnage design

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['username']))
+if (!isset($_SESSION['nID']))
     header("Location: login.php");?>
 <!DOCTYPE html>
 <html lang="en">
@@ -122,31 +122,11 @@ if (!isset($_SESSION['username']))
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
-                        <?php
-                    $servername = "localhost";
-                    $username = "gpcvcsjfun";
-                    $password = "Chamath@1999";
-                    $dbname = "gpcvcsjfun";
-                    // Create connection
-                    $conn = new mysqli("localhost", "gpcvcsjfun", "Chamath@1999", "gpcvcsjfun");
-                    $sqll = "SELECT  * from sales_stats WHERE month='Mar' ";
-                    if (mysqli_query($conn, $sqll))
-{
-echo "";
-}
-else
-{
-echo "Error: " . $sqll . "<br>" . mysqli_error($conn);
-}
-$result = mysqli_query($conn, $sqll);
-if (mysqli_num_rows($result) > 0)
-{while($row = mysqli_fetch_assoc($result))
-    {
-    ?>
+                        
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body"><?php echo $row['Vistors']; ?> visitors</div>
+                                    <div class="card-body"></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -155,18 +135,11 @@ if (mysqli_num_rows($result) > 0)
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body"><?php echo $row['revenue'];?> revenue</div>
+                                    <div class="card-body"></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                        <?php
-}
-}
-else
-{
-echo '0 results';
-}
-?>
+                                        
                                     </div>
                                 </div>
                             </div>
