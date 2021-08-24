82814c1101457516b9124ab8541bde538a97638a
kkchamath17 <kk.chamath@hotmail.com>
2021-08-24 23:36:15 +0530

    after fixing emding

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -151,10 +151,18 @@ if (mysqli_num_rows($result) > 0)
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-body"><?php echo $row['revenue'];?> revenue</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <?php
+}
+}
+else
+{
+echo '0 results';
+}
+?>
                                     </div>
                                 </div>
                             </div>
