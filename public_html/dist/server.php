bb9e1f5bce51844a9083c6d6e8ae076ab193aa40
kkchamath17 <kk.chamath@hotmail.com>
2021-08-25 00:07:29 +0530

    login fix

--- public_html/dist/server.php
+++ public_html/dist/server.php
@@ -64,6 +64,8 @@ if (isset($_POST['login_user'])) {
     $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $results = mysqli_query($db, $query);
     if (mysqli_num_rows($results) == 1) {
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
     header("Location: index.php");
     }else {
     array_push($errors, "Wrong username/password combination");
