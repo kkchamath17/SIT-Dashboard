ae73223ba3e33dc8d3258f7560c0f6d5dcd84dd7
kkchamath17 <kk.chamath@hotmail.com>
2021-08-24 23:40:52 +0530

    after colum

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -142,7 +142,7 @@ if (mysqli_num_rows($result) > 0)
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body"><?php echo $row['Vistors']; ?> visitors</div>
+                                    <div class="card-body"><?php echo $column['Vistors']; ?> visitors</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
