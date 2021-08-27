a8b999a7d4d9b94126afc93a28b4846fd5f3eb30
kkchamath17 <kk.chamath@hotmail.com>
2021-08-27 09:22:15 +0530

    sum

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -126,8 +126,7 @@ if (!isset($_SESSION['username']))
                     // Create connection
                     $conn = new mysqli("localhost", "gpcvcsjfun", "Chamath@1999", "gpcvcsjfun");
                     $sqll = "SELECT  * from sales_stats WHERE month='Mar' ";
-                    $sum = "SELECT *  from sales_stats SUM(revenue)";
-                    if (mysqli_query($conn, $sqll, $sum))
+                    if (mysqli_query($conn, $sqll))
 {
 echo "";
 }
@@ -135,7 +134,7 @@ else
 {
 echo "Error: " . $sqll . "<br>" . mysqli_error($conn);
 }
-$result = mysqli_query($conn, $sqll, $sum);
+$result = mysqli_query($conn, $sqll);
 if (mysqli_num_rows($result) > 0)
 {while($row = mysqli_fetch_assoc($result))
     {
@@ -143,7 +142,7 @@ if (mysqli_num_rows($result) > 0)
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body"><?php echo $sum; ?> visitors</div>
+                                    <div class="card-body"><?php echo $row['Vistors_sum']; ?> visitors</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
