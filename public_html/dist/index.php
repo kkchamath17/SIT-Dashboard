8bf272b3e9423fae7d5aafce49d26d80b299797f
kkchamath17 <kk.chamath@hotmail.com>
2021-08-27 10:53:52 +0530

    sum

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -168,7 +168,7 @@ echo '0 results';
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
+                                    <div class="card-body"><?php echo $row['Vistors']; ?> visitors</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
