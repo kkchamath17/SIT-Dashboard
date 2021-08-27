e8b6624c338341c57e04ee9f0795165e45e19607
kkchamath17 <kk.chamath@hotmail.com>
2021-08-27 10:12:30 +0530

    sum

--- public_html/dist/index.php
+++ public_html/dist/index.php
@@ -125,26 +125,12 @@ if (!isset($_SESSION['username']))
                     $dbname = "gpcvcsjfun";
                     // Create connection
                     $conn = new mysqli("localhost", "gpcvcsjfun", "Chamath@1999", "gpcvcsjfun");
-                    $sqll = "SELECT  * from sales_stats WHERE month='Mar' ";
                     $sum = "SELECT sum(revenue) as total FROM sales_stats";
-                    if (mysqli_query($conn, $sqll))
-{
-echo "";
-}
-else
-{
-echo "Error: " . $sqll . "<br>" . mysqli_error($conn);
-}
-$other = mysqli_query($conn, $sum);
-$result = mysqli_query($conn, $sqll);
-if (mysqli_num_rows($result) > 0)
-{while($row = mysqli_fetch_assoc($result))
-    {
     ?>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body"><?php echo $other; ?> visitors</div>
+                                    <div class="card-body"><?php echo $sum; ?> visitors</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -157,14 +143,7 @@ if (mysqli_num_rows($result) > 0)
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                        <?php
-}
-}
-else
-{
-echo '0 results';
-}
-?>
+                                        
                                     </div>
                                 </div>
                             </div>
